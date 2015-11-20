b0984a839e01ac4c6f405293a60272d346ece873
dansel7 <dansel7@hotmail.com>
2015-11-20 11:51:49 -0600

    Inicio CRP NET

---
+++ logs/error.php
@@ -0,0 +1,7 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2015-11-20 16:09:26 UTC
+#Software: Joomla Platform 13.1.0 Stable [ Curiosity ] 24-Apr-2013 00:00 GMT
+
+#Fields: datetime	priority clientip	category	message
+2015-11-20T16:09:26+00:00	INFO ::1	joomlafailure	El usuario y contraseña no coinciden o usted aún no tiene una cuenta.
---
+++ logs/joomla_update.php
@@ -0,0 +1,14 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2015-11-20 16:09:41 UTC
+#Software: Joomla Platform 13.1.0 Stable [ Curiosity ] 24-Apr-2013 00:00 GMT
+
+#Fields: datetime	priority clientip	category	message
+2015-11-20T16:09:41+00:00	INFO ::1	update	Actualización inciciadoa por el usuario Super User (10). La versión antigua es la 3.4.3.
+2015-11-20T16:09:41+00:00	INFO ::1	update	Descargando el archivo desde https://github.com/joomla/joomla-cms/releases/download/3.4.5/Joomla_3.4.x_to_3.4.5-Stable-Patch_Package.zip.
+2015-11-20T16:10:18+00:00	INFO ::1	update	El archivoe Joomla_3.4.x_to_3.4.5-Stable-Patch_Package.zip se ha descargado correctamente.
+2015-11-20T16:10:18+00:00	INFO ::1	update	Iniciando la instalación de la versión nueva.
+2015-11-20T16:10:21+00:00	INFO ::1	update	Finalizando la instalación.
+2015-11-20T16:10:21+00:00	INFO ::1	update	Borrando los archivos y carpetas que han sido eliminados.
+2015-11-20T16:10:24+00:00	INFO ::1	update	Limpieza posterior a la instalación.
+2015-11-20T16:10:24+00:00	INFO ::1	update	La actualización a la versión 3.4.5 se ha completado.
